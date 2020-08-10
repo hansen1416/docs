2846062a87aa3e4b2726cb4bb4fa79af047e3596
hansen1416 <badapplesweetie@gmail.com>
2020-08-10 13:08:25 +0800

    1

--- docker/b.sh
+++ docker/b.sh
@@ -1,9 +1,9 @@
 
 # docker build -t hansen1416/doc .
 
-# docker run -itd --name doc --restart unless-stopped -p 8100:8100 \
-# --mount type=bind,source=/home/hlz/docs/,target=/home/docs \
-# hansen1416/doc
+docker run -itd --name doc --restart unless-stopped -p 8100:8100 \
+--mount type=bind,source=/home/hlz/docs/,target=/home/docs \
+hansen1416/docs
 
 # docker exec -it doc bash
 
